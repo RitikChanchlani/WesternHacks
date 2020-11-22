d7f0bd8f930ba637a3573a8ba4ff8548db3dcadb
AnirudhTiku <62532928+AnirudhTiku@users.noreply.github.com>
2020-11-22 04:11:21 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,116 +1,110 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
-function build_quiz()
-
-function show_results()
-
-build_quiz();
-
-submitButton.addEventListener('click', showResults);
-
-const myQuestions = [
+  var myQuestions = [
     {
       question: "What percent of Canadian parents say their chid has been cyberbullied before?",
+  
       answers: {
-        a: "10%",
-        b: "15%",
-        c: "18%"
+        a: '10',
+        b: '18',
+        c: '15'
       },
-      correctAnswer: "c"
+      correctAnswer: 'b'
     },
     {
       question: "What percent of of Canadian teens with a profile on a social networking site"+ 
-      "have seen mean or inappropriate comments about someone they know?",
-
+      "have seen mean or inappropriate comments about someone they know??",
       answers: {
-        a: "10%",
-        b: "20%",
-        c: "35%"
+        a: '10',
+        b: '20',
+        c: '35'
       },
-      correctAnswer: "c"
-    },
-    {
-      question: "What percent of students say teachers intervene?",
-      answers: {
-        a: "75%",
-        b: "50%",
-        c: "25%",
-      },
-      correctAnswer: "c"
+      correctAnswer: 'c'
     }
   ];
 
-function buildQuiz(){
-    const output = [];
+
+	function showQuestions(questions, quizContainer){
+	    var output = [];
+	    var answers;
+	    for(var i=0; i<questions.length; i++){
+			    answers = [];
+		      for(letter in questions[i].answers){
+			        answers.push(
+				     '<label>'
+					   + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					   + letter + ': '
+					   + questions[i].answers[letter]
+				     + '</label>'
+			);
+		}
+		    output.push(
+			 '<div class="question">' + questions[i].question + '</div>'
+			 + '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}
+	quizContainer.innerHTML = output.join('');
+}
+	showQuestions(questions, quizContainer);
+
+	function showResults(questions, quizContainer, resultsContainer){
+    var output = [];
+    var answers;
   
-    myQuestions.forEach(
-      (currentQuestion, questionNumber) => {
+    for(var i=0; i<questions.length; i++){
+      
+      answers = [];
   
-        const answers = [];
+      for(letter in questions[i].answers){
   
-        for(letter in currentQuestion.answers){
-  
-          answers.push(
-            `<label>
-              <input type="radio" name="question${questionNumber}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-            </label>`
-          );
-                                            }
-  
-        output.push(
-          `<div class="question"> ${currentQuestion.question} </div>
-          <div class="answers"> ${answers.join('')} </div>`
+        answers.push(
+          '<label>'
+            + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+            + letter + ': '
+            + questions[i].answers[letter]
+          + '</label>'
         );
       }
-    );
+  
+      output.push(
+        '<div class="question">' + questions[i].question + '</div>'
+        + '<div class="answers">' + answers.join('') + '</div>'
+      );
+    }
+  
+    quizContainer.innerHTML = output.join('');
+  }
+
+	showQuestions(questions, quizContainer);
+  
+	function showResults(questions, quizContainer, resultsContainer){
+	
+    var answerContainers = quizContainer.querySelectorAll('.answers');
     
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
-      })
-      quizContainer.innerHTML = output.join('');
-
-      function showResults(){
-
-        const answerContainers = quizContainer.querySelectorAll('.answers');
+    var userAnswer = '';
+    var numCorrect = 0;
+    
+    for(var i=0; i<questions.length; i++){
+  
+      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
       
-        let numCorrect = 0;
-      
-        myQuestions.forEach( (currentQuestion, questionNumber) => {
-      
-          const answerContainer = answerContainers[questionNumber];
-          const selector = `input[name=question${questionNumber}]:checked`;
-          const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-      
-          if(userAnswer === currentQuestion.correctAnswer){
-            numCorrect++;
-      
-            answerContainers[questionNumber].style.color = 'lightgreen';
-          }
-          else{
-            answerContainers[questionNumber].style.color = 'red';
-          }
-        });
-      
-        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-        const answerContainers = quizContainer.querySelectorAll('.answers');
-
-        let numCorrect = 0;
-
-        myQuestions.forEach( (currentQuestion, questionNumber) => {
-
-            const answerContainer = answerContainers[questionNumber];
-            const selector = `input[name=question${questionNumber}]:checked`;
-            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-          
-            if(userAnswer === currentQuestion.correctAnswer){
-              numCorrect++;
-          
-              answerContainers[questionNumber].style.color = 'lightgreen';
-            }
-            else{
-              answerContainers[questionNumber].style.color = 'red';
-            }
-                                                                  });
+      if(userAnswer===questions[i].correctAnswer){
+        numCorrect++;
+        
+        answerContainers[i].style.color = 'lightgreen';
+      }
+      else{
+        answerContainers[i].style.color = 'red';
+      }
+    }
+  
+    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+  }
+  submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+  }
+  var quizContainer = document.getElementById('quiz');
+  var resultsContainer = document.getElementById('results');
+  var submitButton = document.getElementById('submit');
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+}
