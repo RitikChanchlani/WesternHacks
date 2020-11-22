6e464d2423f39ebedf2fe431cb77da7d361665bd
kx869 <74390819+kx869@users.noreply.github.com>
2020-11-21 20:42:46 -0500

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -14,7 +14,6 @@ def check_misspelt_profanity(word, dict_entry):
         return True
 
 
-
 def writefile(f1, f2):
     l1 = []
     l2 = []
@@ -41,16 +40,15 @@ class Detector:
         self.not_good_words = []
         self.bad_words = []
         self.filler_words = []
-        self.pronouns = []
-        self.negation_words = []
-        self.bad_emojis = ['🍑', "🖕", '🍆 ']
+        self.pronouns_file = []
+
 
     def checkTweet(self, tweet):
         info = tweet.split()
         for word in info:
             if word in self.filler_words:
                 info.remove(word)
-
+        print(info)
         tweet_bad_words = []
         tweet_not_good_words = []
         tweet_bad_emojis = []
@@ -69,16 +67,21 @@ class Detector:
                         found = True
                         break
             if not found:
-                if word in self.bad_emojis:
+                if word == "🖕":
                     tweet_bad_emojis.append(word)
                     found = True
             if not found:
                 for i in range(len(self.not_good_words)):
                     if word == self.not_good_words[i]:
+                        negation_words_list = []
+                        negation_words_file = open("negation_words.txt", "r")
+                        for word in negation_words_file:
+                            stripped_word = word.strip()
+                            negation_words_list.append(stripped_word)
                         previous_word_index = unaltered_word_index - 1
                         not_counter = 0
                         # continues until the previous word is not a negation word
-                        while info[previous_word_index] in self.negation_words:
+                        while info[previous_word_index] in negation_words_list:
                             not_counter += 1
                             previous_word_index -= 1
                         # ex: "not not ___" does not count
@@ -86,9 +89,10 @@ class Detector:
                             found = True
                             break
                         else:
-                            tweet_not_good_words.append(self.not_good_words[i])
-                            found = True
-                            break
+                            if (info[previous_word_index] in self.pronouns_file) or (info[unaltered_word_index + 1] in self.pronouns_file):
+                                tweet_not_good_words.append(self.not_good_words[i])
+                                found = True
+                                break
             if not found:
                 for entry in self.bad_words:
                     if word.find(entry) != -1:
@@ -113,7 +117,7 @@ class Detector:
 
         return [tweet_bad_words, tweet_not_good_words, tweet_bad_emojis]
 
-    def load_dictionary(self, bad_words_file, not_good_words_file, good_words_file, filler_words_file, pronouns_file, negation_words_file):
+    def load_dictionary(self, bad_words_file, not_good_words_file, good_words_file, filler_words_file, pronouns_file):
         try:
             with open(bad_words_file, "r") as reader:
                 lines = reader.readlines()
@@ -139,12 +143,7 @@ class Detector:
                 lines = reader.readlines()
                 for line in lines:
                     line = line.replace('\n', '')
-                    self.pronouns.append(line)
-            with open(negation_words_file, "r") as reader:
-                lines = reader.readlines()
-                for line in lines:
-                    line = line.replace('\n', '')
-                    self.negation_words.append(line)
+                    self.pronouns_file.append(line)
         except FileNotFoundError:
             pass
 
@@ -152,7 +151,6 @@ class Detector:
 
 
 d = Detector()
-d.load_dictionary("restricted_words.txt", "concerning_words.txt", "dictionary.txt", "filler_words.txt", "pronouns.txt"
-                  , "negation_words.txt")
-results = d.checkTweet("ugly wrinkle vagina 🖕 ass i love little girls")
+d.load_dictionary("restricted_words.txt", "concerning_words.txt", "dictionary.txt", "filler_words.txt", "pronouns_file.txt")
+results = d.checkTweet("so not not wrong you not not not wrinkle vagina not 🖕 ass")
 print(results)
