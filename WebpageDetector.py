f268d281f501dd743d0d7af5409533a4ca0f3566
jasonzhang2002 <72712849+jasonzhang2002@users.noreply.github.com>
2020-11-21 23:46:28 -0500

    Update WebpageDetector.py

--- WebpageDetector.py
+++ WebpageDetector.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def check_misspelt_profanity(word, dict_entry):
     consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k',
                   'l', 'm', 'n', 'p', 'q', 'r', 's', 't',
@@ -37,6 +38,7 @@ def writefile(f1, f2):
         f.write(word)
     f.close()
 
+
 class Detector:
     def __init__(self):
         self.good_words = []
@@ -44,7 +46,6 @@ class Detector:
         self.bad_words = []
         self.filler_words = []
         self.pronouns_file = []
-
 
     def checkTweet(self, tweet):
         info = tweet.split()
@@ -92,7 +93,8 @@ class Detector:
                             found = True
                             break
                         else:
-                            if (info[previous_word_index] in self.pronouns_file) or (info[unaltered_word_index + 1] in self.pronouns_file):
+                            if (info[previous_word_index] in self.pronouns_file) or (
+                                    info[unaltered_word_index + 1] in self.pronouns_file):
                                 tweet_not_good_words.append(self.not_good_words[i])
                                 found = True
                                 break
@@ -117,7 +119,7 @@ class Detector:
                 for entry in self.bad_words:
                     if check_misspelt_profanity(word, entry):
                         tweet_bad_words.append(entry)
-        
+
         output = ''
         output += "List of vulgar words: "
         for word in tweet_bad_words:
@@ -159,37 +161,39 @@ class Detector:
                     self.pronouns_file.append(line)
         except FileNotFoundError:
             pass
-            
-        def check_webpage(self, url):
-            res = requests.get(url)
-            html_page = res.content
-            soup = BeautifulSoup(html_page, 'html.parser')
-            text = soup.find_all(text=True)
 
-            output = ''
-            blacklist = [
-                '[document]',
-                'noscript',
-                'header',
-                'html',
-                'meta',
-                'head',
-                'input',
-                'script',
-                # there may be more elements you don't want, such as "style", etc.
-            ]
+    def check_webpage(self, url):
+        res = requests.get(url)
+        html_page = res.content
+        soup = BeautifulSoup(html_page, 'html.parser')
+        text = soup.find_all(text=True)
 
-            for t in text:
-                if t.parent.name not in blacklist:
-                    output += '{} '.format(t)
+        output = ''
+        blacklist = [
+            '[document]',
+            'noscript',
+            'header',
+            'html',
+            'meta',
+            'head',
+            'input',
+            'script',
+            # there may be more elements you don't want, such as "style", etc.
+        ]
 
-            return output
+        for t in text:
+            if t.parent.name not in blacklist:
+                output += '{} '.format(t)
 
-#user_text = input("Please enter the text you would like to scan:\n")
+        return output
+
+
+# user_text = input("Please enter the text you would like to scan:\n")
 
 
 d = Detector()
-d.load_dictionary("restricted_words.txt", "concerning_words.txt", "dictionary.txt", "filler_words.txt", "pronouns_file.txt")
+d.load_dictionary("restricted_words.txt", "concerning_words.txt", "dictionary.txt", "filler_words.txt",
+                  "pronouns_file.txt")
 string = d.check_webpage("https://www.urbandictionary.com")
 results = d.checkTweet(string)
 print(results)
